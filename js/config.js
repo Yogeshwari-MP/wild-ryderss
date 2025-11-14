ff494d3aeff176a98e5b5e3a1f4cb86713b06d0b
Yogeshwari-MP <mpyogeshwari03@gmail.com>
2025-11-14 12:17:19 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,8 +5,9 @@ window._config = {
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://whefm7p3pi.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://ddtm8ie7wj.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
