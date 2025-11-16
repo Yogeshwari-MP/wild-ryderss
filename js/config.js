1dfc0c253085991e494d11a6f7ecc4c9ed8d7f23
Yogeshwari-MP <mpyogeshwari03@gmail.com>
2025-11-16 15:40:40 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_3Bw6U3FfN', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3q3lm0kqots5p9lgp1hm24bgdh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_CcOmcQs3f', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '20jsccoaoor00i83k4o1nocl1u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://ddtm8ie7wj.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
